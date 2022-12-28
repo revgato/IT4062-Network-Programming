67d91f649dcced146592908e4eea982495a8011a
revgato <locchoihay147@gmail.com>
2022-12-28 17:20:22 +0700

    init

---
+++ week7/message.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Define structure of message
+typedef struct message_t{
+    char username[20];
+    char message[100];
+}message;
--- week7/user_manage.h
+++ week7/user_manage.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Define structure of user
 
@@ -11,6 +12,45 @@ typedef struct user_t{
 // Define list of user
 
 typedef struct user_list_t{
-    user *data;
+    user usr;
     struct user_list_t *next;
 } user_list;
+
+// Read user from file
+void read_user_data(user_list **head){
+    FILE *f = fopen("taikhoan.txt", "r");
+    if(f == NULL){
+        printf("Cannot open file taikhoan.txt\n");
+        exit(1);
+    }
+    user_list *temp = *head;
+    while(!feof(f)){
+        user_list *new_node = (user_list *)malloc(sizeof(user_list));
+        new_node->next = NULL;
+        fscanf(f, "%s %s", new_node->usr.username, new_node->usr.password);
+        if(*head == NULL){
+            *head = new_node;
+            temp = *head;
+        }
+        else{
+            while(temp->next != NULL){
+                temp = temp->next;
+            }
+            temp->next = new_node;
+            temp = temp->next;
+        }
+    }
+    fclose(f);
+}
+
+// Login validation
+int login(user_list *head, char *username, char *password){
+    user_list *temp = head;
+    while(temp != NULL){
+        if(strcmp(temp->usr.username, username) == 0 && strcmp(temp->usr.password, password) == 0){
+            return 1;
+        }
+        temp = temp->next;
+    }
+    return 0;
+}
